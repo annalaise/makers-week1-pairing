166931ec4e58d7cdbc84f0437b9c80532c92160e
annalaise <annalaise.gibbons@gmail.com>
2017-07-03 16:20:40 +0100

    adding first draft of test file

---
+++ spec/arraypair_spec.rb
@@ -0,0 +1,7 @@
+# require 'filenamegoeshere'
+
+describe 'first array test' do
+  it 'removes 0th element, pushes element to end' do
+    expect(arraypair([1, 2, 3, 4, 5])). to eq '[2, 3, 4, 5, 6]'
+  end
+end

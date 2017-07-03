516e3d7d665fe0d4075653cced7306f07081141b
Graham Cottridge <codinggc@gmail.com>
2017-07-03 17:26:08 +0100

    second array

--- spec/arraypair_spec.rb
+++ spec/arraypair_spec.rb
@@ -4,4 +4,7 @@ describe 'first array test' do
   it 'removes 0th element, pushes element to end' do
     expect(array_1([1, 2, 3, 4, 5])). to eq [2, 3, 4, 5, 6]
   end
+  it 'sort elements' do
+    expect(array_2([1, 3, 5, 4, 2])). to eq [1, 2, 3, 4, 5]
+  end
 end

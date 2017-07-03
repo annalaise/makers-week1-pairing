9fdc386e841f3138e68264b7b5ad741879699ec8
annalaise <annalaise.gibbons@gmail.com>
2017-07-03 16:26:51 +0100

    updated test file

--- spec/arraypair_spec.rb
+++ spec/arraypair_spec.rb
@@ -2,6 +2,6 @@
 
 describe 'first array test' do
   it 'removes 0th element, pushes element to end' do
-    expect(arraypair([1, 2, 3, 4, 5])). to eq '[2, 3, 4, 5, 6]'
+    expect(arraypair([1, 2, 3, 4, 5])). to eq [2, 3, 4, 5, 6]
   end
 end
